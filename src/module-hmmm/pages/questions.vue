28efbe77bbaecd27a231ebb41bb6e49b9e9a3976
EVOL-SFX <56402321+EVOL-SFX@users.noreply.github.com>
2019-11-22 21:11:40 +0800

    Update questions.vue
    基础题库管理

--- src/module-hmmm/pages/questions.vue
+++ src/module-hmmm/pages/questions.vue
@@ -1,14 +1,73 @@
 <template>
   <div class="dashboard-container">
-    <div class="app-container">基础题库管理</div>
+    <div class="app-container">
+      
+      <el-row>
+        <el-col :span="6">
+          学科：
+          <el-select v-model="searchForm.subjectID">
+            <el-option v-for="item in subjectIDList" :key="item.value" :value="item.value" :label="item.label">
+            </el-option>
+          </el-select>
+        </el-col>
+        <el-col :span="6">
+          难度：
+          <el-select v-model="searchForm.difficulty">
+            <el-option v-for="item in difficultyList" :key="item.value" :value="item.value" :label="item.label">
+            </el-option>
+          </el-select>
+        </el-col>
+        <el-col :span="6">
+          试题类型：
+          <el-select v-model="searchForm.questionType" style="width:140px;">
+            <el-option v-for="item in questionTypeList" :key="item.value" :value="item.value" :label="item.label">
+            </el-option>
+          </el-select>
+        </el-col>
+      </el-row>
+      
+    </div>
   </div>
 </template>
 
 <script>
+// 学科
+// as给导入的成员设置别名
+import {simple} from '@/api/hmmm/subjects'
+import {
+  difficulty as difficultyList, 
+  questionType as questionTypeList} 
+  from '@/api/hmmm/constants' // 常量数据
 export default {
   name: 'QuestionsList',
   data() {
-    return {}
+    return {
+      // 定义各个搜索表单域的数据展示成员
+      subjectIDList: [],
+      difficultyList, // 简易成员赋值(difficultyList:difficultyList)
+      questionTypeList, // 简易成员赋值
+
+      // 定义搜索数据对象
+      searchForm: {
+        subjectID: '',
+        difficulty: '',
+        questionType: ''
+      }
+    }
+  },
+  created() {
+    // 学科
+    this.getSubjectIDList()
+  },
+
+  methods: {
+    // 获得 学科 下拉列表数据
+    async getSubjectIDList() {
+      var rst = await simple()
+      // console.log(rst)
+      // 把获得到的学科信息赋予给 subjectIDList 成员
+      this.subjectIDList = rst.data
+    }
   }
 }
 </script>
